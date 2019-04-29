8ff5d157af5c0637199948ffa5059e9ca6bbab1c
manoja328 <manoja328@gmail.com>
2019-04-29 17:59:52 -0400

    depend

---
+++ dependencies/vlfeat/toolbox/mex/mexa64/vl_fisher.d
@@ -0,0 +1,7 @@
+toolbox/mex/mexa64/vl_fisher.mexa64 toolbox/mex/mexa64/vl_fisher.d: \
+ toolbox/fisher/vl_fisher.c vl/fisher.h vl/generic.h vl/host.h \
+ vl/random.h toolbox/mexutils.h \
+ /usr/local/MATLAB/R2017b/extern/include/mex.h \
+ /usr/local/MATLAB/R2017b/extern/include/matrix.h \
+ /usr/local/MATLAB/R2017b/extern/include/tmwtypes.h vl/generic.h \
+ vl/array.h vl/stringop.h
