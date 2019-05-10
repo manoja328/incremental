97a030538b67055451f9ff5ba0f01ae84ea89063
manoja328 <manoja328@gmail.com>
2019-05-10 12:44:43 -0400

    change how the outputs were shaped

--- eval_pool.m
+++ eval_pool.m
@@ -1,29 +1,98 @@
-function outputs = eval_pool(net, imdb)
+function outputs = eval_pool(net, imdb, pos)
 
 if strcmp(net.device, 'cpu')
     net.move('gpu');
 end
 
-net.conserveMemory = 0;
+% net.conserveMemory = 0;
 nsamp = 1;
 outputs = [];
-while nsamp <= size(imdb.images.data, 4)
-    step = min(256, size(imdb.images.data, 4) - nsamp+1);
-    images = gpuArray(imdb.images.data(:, :, :, nsamp:nsamp+step-1));
-    inputs = {'image', images};
+train = [] ;
+% if isempty(train), train = find(imdb.images.set==1) ; end
+
+% fprintf("pos %d\n",length(pos));
+train = pos;
+% classtr = unique(train.images.labels);
+% fprintf("class %d\n",classtr);
+
+imdbnew = {};
+imdbnew.folders = imdb.images.folders(train);
+imdbnew.name = imdb.images.name(train);
+
+meta = net.meta;
+opts.numFetchThreads = 12 ;
+opts.numAugments = 1 ;
+
+bopts.numThreads = opts.numFetchThreads ;
+bopts.imageSize = meta.normalization.imageSize ;
+bopts.border = meta.normalization.border ;
+bopts.averageImage = meta.normalization.averageImage ;
+bopts.rgbVariance = meta.augmentation.rgbVariance ;
+bopts.transformation = meta.augmentation.transformation ;
+bopts.numAugments = opts.numAugments ; 
+
+%  fprintf('%d to forward \n', size(train, 2));
+while nsamp <= size(train, 2)
+    step = min(256, size(train, 2) - nsamp+1);
+    
+    fprintf('%d out of %d to forward \n',nsamp, size(train, 2))
+%     tic
+    batch = nsamp:1:nsamp+step-1;
+    
+    
+    images = strcat('data/ILSVRC2012/train/',imdbnew.folders(batch),filesep,imdbnew.name(batch));
+    images = cnn_imagenet_get_batch(images,bopts, ...
+                              'prefetch', nargout == 0, ...
+                              'transformation', 'none') ;
+    images = gpuArray(images) ;
+%     toc;
+%     tic;
+% inputs = {'data', im, 'label', labels} ;
+    
+%     images = gpuArray(imdb.images.data(:, :, :, nsamp:nsamp+step-1));
+    inputs = {'data', images};
     net.eval(inputs) ;
     nsamp = nsamp + step;
     
     % Gather results.
     index = strfind({net.layers.name}, 'pool_final');
     index = find(not(cellfun('isempty', index)));
+    npos = length(index);
     
-    % Concat results.
-    x = squeeze(gather(net.vars(net.layers(index(1)).outputIndexes(1)).value));
-    outputs = cat(2, outputs, x);
+   for lix = 1:npos
+      x = squeeze(gather(net.vars(net.layers(index(lix)).outputIndexes(1)).value));
+      outputs = cat(2, outputs, x);
+   end
+    
 end
 
 if strcmp(net.device, 'gpu')
     net.move('cpu');
 end
 
+
+% 
+% % -------------------------------------------------------------------------
+% function inputs = getDagNNBatch(opts, useGpu, imdb, batch)
+% % -------------------------------------------------------------------------
+% images = strcat([imdb.imageDir filesep], imdb.images.name(batch)) ;
+% isVal = ~isempty(batch) && imdb.images.set(batch(1)) ~= 1 ;
+% 
+% if ~isVal
+%   % training
+%   im = cnn_imagenet_get_batch(images, opts, ...
+%                               'prefetch', nargout == 0) ;
+% else
+%   % validation: disable data augmentation
+%   im = cnn_imagenet_get_batch(images, opts, ...
+%                               'prefetch', nargout == 0, ...
+%                               'transformation', 'none') ;
+% end
+% 
+% if nargout > 0
+%   if useGpu
+%     im = gpuArray(im) ;
+%   end
+%   labels = imdb.images.label(batch) ;
+%   inputs = {'data', im, 'label', labels} ;
+% end
--- eval_softmax.m
+++ eval_softmax.m
@@ -6,11 +6,55 @@ end
 
 net.conserveMemory = 0;
 nsamp = 1;
-outputs = {};
-while nsamp <= size(imdb.images.data, 4)
-    step = min(256, size(imdb.images.data, 4) - nsamp+1);
-    images = gpuArray(single(imdb.images.data(:, :, :, nsamp:nsamp+step-1)));
-    inputs = {'image', images};
+outputs = [];
+
+
+% outputs = [];
+train = [] ;
+% if isempty(train), train = find(imdb.images.set==1) ; end
+% if isempty(train), train = find(imdb.images.set==1) ; end
+% if isempty(train), train = find(imdb.images.set==1) ; end
+if isempty(train), train = [1:size(imdb.images.set,2)] ; end
+
+meta = net.meta;
+opts.numFetchThreads = 12 ;
+opts.numAugments = 1 ;
+
+bopts.numThreads = opts.numFetchThreads ;
+bopts.imageSize = meta.normalization.imageSize ;
+bopts.border = meta.normalization.border ;
+bopts.averageImage = meta.normalization.averageImage ;
+bopts.rgbVariance = meta.augmentation.rgbVariance ;
+bopts.transformation = meta.augmentation.transformation ;
+bopts.numAugments = opts.numAugments ; 
+
+imdb.images.fullname = {};
+trpos = find(imdb.images.set == 1 );
+valpos = find(imdb.images.set == 3 );
+imdb.images.fullname(trpos) = strcat('data/ILSVRC2012/train/',imdb.images.folders(trpos),filesep,imdb.images.name(trpos));
+imdb.images.fullname(valpos) = strcat('data/ILSVRC2012/val/',imdb.images.folders(valpos),filesep,imdb.images.name(valpos));
+
+
+% while nsamp <= size(imdb.images.data, 4)
+while nsamp <= size(train, 2)
+%     step = min(256, size(imdb.images.data, 4) - nsamp+1);\
+
+    step = min(256, size(train, 2) - nsamp+1);
+    fprintf('%d out of %d to forward \n',nsamp, size(train, 2))
+    
+    batch = nsamp:1:nsamp+step-1;
+    
+    images = imdb.images.fullname(batch);
+    images = cnn_imagenet_get_batch(images,bopts, ...
+                              'prefetch', nargout == 0, ...
+                              'transformation', 'none') ;
+    images = gpuArray(images) ;   
+    
+                          
+    
+%     images = gpuArray(single(imdb.images.data(:, :, :, nsamp:nsamp+step-1)));
+%     inputs = {'image', images};
+    inputs = {'data', images};
     net.eval(inputs) ;
     nsamp = nsamp + step;
     
@@ -23,15 +67,15 @@ while nsamp <= size(imdb.images.data, 4)
     index2 = find(not(cellfun('isempty', index2)));
     nposFC = length(index2);
 
-    if isempty(outputs)
-        outputs = cell(1, nposFC);
-    end
+%     if isempty(outputs)
+%         outputs = cell(1, nposFC);
+%     end
     
     lastPos = 1;
     for lix = 1:npos
         if ~strcmp(net.layers(index(lix)).name, 'softmax_global') && ~strcmp(net.layers(index(lix)).name, 'softmax_old')
             x = squeeze(gather(net.vars(net.layers(index(lix)).outputIndexes(1)).value));
-            outputs{lastPos} = cat(2, outputs{lastPos}, x);
+            outputs = cat(2, outputs, x);
             lastPos = lastPos + 1;
         end
     end % lix
@@ -40,4 +84,3 @@ end
 if strcmp(net.device, 'gpu')
     net.move('cpu');
 end
-
